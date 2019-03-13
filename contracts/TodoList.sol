7e98cb424b27451029d92c3a557ab58ff8c3308c
unknown <hexoindia@gmail.com>
2019-03-14 02:09:25 +0530

    Initial Commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -17,6 +17,11 @@ contract TodoList {
     bool completed
   );
 
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
   constructor() public {
     createTask("This is a default task.");
   }
@@ -27,6 +32,13 @@ contract TodoList {
     emit TaskCreated(taskCount, _content, false);
   }
 
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
 
+    tasks[_id] = _task;
+
+    emit TaskCompleted(_id, _task.completed);
+  }
 
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -1,7 +1,7 @@
 const TodoList = artifacts.require('./TodoList.sol')
 
 contract('TodoList', (accounts) => {
-    
+
     before(async () => {
         this.todoList = await TodoList.deployed()
     })
@@ -33,7 +33,13 @@ contract('TodoList', (accounts) => {
         assert.equal(event.completed, false)
     })
 
-
-
+    it('toggles task completion', async () => {
+        const result = await this.todoList.toggleCompleted(1)
+        const task = await this.todoList.tasks(1)
+        assert.equal(task.completed, true)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 1)
+        assert.equal(event.completed, true)
+    })
 
 })
